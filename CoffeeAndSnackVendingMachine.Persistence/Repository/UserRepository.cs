79c3a875ee83aff2d01ad152974f8220b7798116
im48178 <80419440+im48178@users.noreply.github.com>
2021-04-21 17:18:32 +0200

    Add files via upload

---
+++ CoffeeAndSnackVendingMachine.Persistence/Repository/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using CoffeeAndSnackVendingMachine.Domain.Entities;
+
+namespace CoffeeAndSnackVendingMachine.Persistence.Repository
+{
+    public class UserRepository : BaseRepository<User>
+    {
+        public UserRepository(IdentityDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
